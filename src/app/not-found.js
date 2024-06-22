8ed6dc1cb36bc41b6eab530535e3a0f2a7d3ef3a
akbr11 <ciremaiindah3078@gmail.com>
2024-06-22 16:53:20 +0700

    feat: UI theme

---
+++ src/app/not-found.js
@@ -0,0 +1,22 @@
+import { FileSearch } from "@phosphor-icons/react/dist/ssr";
+import Link from "next/link";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="min-h-screen max-w-xl mx-auto flex justify-center items-center">
+      <div className="flex justify-center items-center flex-col">
+        <FileSearch size={44} className="text-color-accent" />
+        <h1 className="text-color-accent text-4xl font-bold">Not Found</h1>
+        <Link
+          href={"/"}
+          className="text-color-primary hover:text-color-accent transition-all underline"
+        >
+          Kembali
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
